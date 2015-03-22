17e32ccd2ef6be024d4964de5fb345f21df3a33a
Miraj <rthonsgard@gmail.com>
2015-03-22 13:36:59 -0500

    Reupload migrations

--- db/migrate/20150322173220_add_project_id_to_project_site_information.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIdToProjectSiteInformation < ActiveRecord::Migration
-  def change
-    add_column :project_site_informations, :project_id, :integer
-  end
-end
--- db/migrate/20150322173626_remove_project_type_information_id_from_project_site_information.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProjectTypeInformationIdFromProjectSiteInformation < ActiveRecord::Migration
-  def change
-    remove_column :project_site_informations, :project_type_information_id, :integer
-  end
-end
