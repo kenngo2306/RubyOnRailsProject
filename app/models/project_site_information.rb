23400b977c2327d81a95b78ba9cb85289b67ac0f
Ken <cpngo@uh.edu>
2015-03-25 21:16:13 -0500

    add type informations to site information show page

--- app/models/project_site_information.rb
+++ app/models/project_site_information.rb
@@ -2,6 +2,7 @@ class ProjectSiteInformation < ActiveRecord::Base
 
   belongs_to :project
   belongs_to :project_site
+  has_many :project_type_informations
   has_many :project_types, through: :project_type_informations
   has_many :utilities, through: :project_utilities
 
