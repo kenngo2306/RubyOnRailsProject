8670d694bd2825b237961c2b391873568a740fec
Ken <cpngo@uh.edu>
2015-03-31 02:31:44 -0500

    take out turbolink, tried to update project site info table in project show page

--- app/models/project_site.rb
+++ app/models/project_site.rb
@@ -2,4 +2,7 @@ class ProjectSite < ActiveRecord::Base
 
   belongs_to :state
   # has_many :projects, through: :project_site_informations
+  has_many :project_site_informations
+  accepts_nested_attributes_for :project_site_informations
+  has_many :projects, through: :project_site_informations
 end
--- app/models/project_site_information.rb
+++ app/models/project_site_information.rb
@@ -1,10 +1,12 @@
 class ProjectSiteInformation < ActiveRecord::Base
-
-  belongs_to :project
   belongs_to :project_site
+  belongs_to :project
+
   has_many :project_type_informations
   has_many :project_types, through: :project_type_informations
   has_many :utilities, through: :project_utilities
 
-  accepts_nested_attributes_for :project_site
+  def self.getAllFromProjectID(project_id)
+    ProjectSiteInformation.where('project_id = ?', project_id)
+  end
 end
