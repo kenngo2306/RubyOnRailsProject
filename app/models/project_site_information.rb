edf4a345b4a08f796580a9fe08bcda3ebd02e326
Ken <cpngo@uh.edu>
2015-04-08 21:59:06 -0500

    fix error

--- app/models/project_site_information.rb
+++ app/models/project_site_information.rb
@@ -7,10 +7,11 @@ class ProjectSiteInformation < ActiveRecord::Base
   has_many :project_utilities
   has_many :utilities, through: :project_utilities
 
-  validates_presence_of :area_length, :area_width, :area_description, :project_id
+  # validates_presence_of :area_length, :area_width, :area_description, :project_id
+
   validates_length_of :area_length, :area_width, maximum: 5
-  validates_length_of :area_description, :soil_conditions, maximum: 150
-  validates_uniqueness_of :project_id
+  validates_length_of :area_description, :soil_condition, maximum: 150
+  # validates_uniqueness_of :project_id
 
   def self.getAllFromProjectID(project_id)
     ProjectSiteInformation.where('project_id = ?', project_id)
