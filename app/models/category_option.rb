be66649fdc2eb81a3d0132feefaefe3055ee7171
Ken <cpngo@uh.edu>
2015-04-12 12:37:10 -0500

    fix length of category and category option

--- app/models/category_option.rb
+++ app/models/category_option.rb
@@ -5,5 +5,5 @@ class CategoryOption < ActiveRecord::Base
   has_many :project_type_informations, through: :category_option_selections
 
   validates_presence_of :category_option_name, :category_id
-  validates_length_of :category_option_name, maximum: 30
+  validates_length_of :category_option_name, maximum: 40
 end
